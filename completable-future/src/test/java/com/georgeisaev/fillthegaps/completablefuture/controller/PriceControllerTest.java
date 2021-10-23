6ec0df912290485ec5c6880104b9425c0628cb6a
Исаев Георгий Геннадьевич <isaev.georgii@otr.ru>
2021-10-23 16:52:10 +0300

    completable future task - unit tests

---
+++ completable-future/src/test/java/com/georgeisaev/fillthegaps/completablefuture/controller/PriceControllerTest.java
@@ -0,0 +1,63 @@
+package com.georgeisaev.fillthegaps.completablefuture.controller;
+
+import com.georgeisaev.fillthegaps.completablefuture.service.PriceAggregatorService;
+import com.georgeisaev.fillthegaps.completablefuture.service.PriceRetrieverService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.LongStream;
+
+import static com.georgeisaev.fillthegaps.completablefuture.controller.PriceControllerTest.EXPECTED_LOWEST_PRICE;
+import static org.mockito.Mockito.when;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Import(TestConfig.class)
+@WebMvcTest(PriceController.class)
+class PriceControllerTest {
+
+    protected static final double EXPECTED_LOWEST_PRICE = 88.88d;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldRetrieveLowestPrice() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/items/1/prices/lowest"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(String.valueOf(EXPECTED_LOWEST_PRICE)));
+    }
+
+}
+
+@Configuration
+@Import(PriceAggregatorService.class)
+class TestConfig {
+
+    @Bean
+    @Primary
+    PriceRetrieverService priceRetrieverService() {
+        PriceRetrieverService priceRetrieverService = Mockito.mock(PriceRetrieverService.class);
+        LongStream.rangeClosed(1, 100).forEach(shopId -> {
+            when(priceRetrieverService.findPrice(1L, shopId))
+                    .thenReturn(ThreadLocalRandom.current().nextDouble(90d, 10_000d));
+        });
+        when(priceRetrieverService.findPrice(1L, 3))
+                .thenReturn(EXPECTED_LOWEST_PRICE);
+        return priceRetrieverService;
+    }
+
+}
