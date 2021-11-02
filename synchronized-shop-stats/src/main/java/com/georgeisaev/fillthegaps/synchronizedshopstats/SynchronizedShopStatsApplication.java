748d78e9759d299aaf1a3548de9a740a1a23260b
Исаев Георгий Геннадьевич <isaev.georgii@otr.ru>
2021-11-02 09:01:24 +0300

    synchronized

---
+++ synchronized-shop-stats/src/main/java/com/georgeisaev/fillthegaps/synchronizedshopstats/SynchronizedShopStatsApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.fillthegaps.synchronizedshopstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizedShopStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronizedShopStatsApplication.class, args);
+    }
+
+}
---
+++ synchronized-shop-stats/src/test/java/com/georgeisaev/fillthegaps/synchronizedshopstats/SynchronizedShopStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.fillthegaps.synchronizedshopstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizedShopStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
