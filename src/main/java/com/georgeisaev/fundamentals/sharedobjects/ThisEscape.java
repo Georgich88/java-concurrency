c9602725b1a62bbd4b3b90115dc22e7bd32a930b
Исаев Георгий <isaev.georgii@otr.ru>
2021-11-28 20:22:23 +0300

    escaping example

---
+++ src/main/java/com/georgeisaev/fundamentals/sharedobjects/ThisEscape.java
@@ -0,0 +1,59 @@
+package com.georgeisaev.fundamentals.sharedobjects;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+public class ThisEscape {
+
+    private String name;
+
+    public ThisEscape() {
+        this.name = "ThisEscape " + Thread.currentThread().getName();
+    }
+
+    public ThisEscape(EventSource source) {
+        this();
+        source.registerListener(
+                new EventListener() {
+                    public void onEvent(Event e) {
+                        log.info("Event {}", e);
+                        doSomething(e);
+                    }
+                });
+    }
+
+    void doSomething(Event event) {
+        log.info("Event {}", event);
+    }
+
+    public static void main(String[] args) {
+        ThisEscape t = new ThisEscape(new EventSource());
+    }
+
+}
+
+@Slf4j
+@Data
+class EventSource {
+
+    void registerListener(EventListener eventListener) {
+        log.info("EventListener {}", eventListener);
+    }
+
+}
+
+interface EventListener {
+
+    void onEvent(Event e);
+
+}
+
+@Slf4j
+@Data
+class Event {
+
+    private String name;
+
+}
